48481da6ebd24bbff3f8e24e49759163843c63b2
leesonvee <weils@bsoft.com.cn>
2020-05-20 08:02:59 +0800

    最终版电子票据代码

---
+++ src/main/java/com/vee/inter/Constants.java
@@ -0,0 +1,9 @@
+package com.vee.inter;
+
+/**
+ * Created by weilishan on 2020/5/16.
+ */
+public class Constants {
+    public static final String MZ = "MZ";
+    public static final String ZY = "ZY";
+}
